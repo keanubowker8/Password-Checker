e61debc31ae6d2aade97aa324cdd72fa9f786181
keanubowker8 <keanu.bowker@umuzi.org>
2019-08-22 11:20:09 +0200

    first commit

--- password.py
+++ password.py
@@ -1,28 +1,36 @@
 def password_is_valid(password):
-	if (len(password)>8 and password != None and 
-		any(c.islower() for c in password) and
-		any(c.isupper() for c in password) and
-		any(i.isdigit() for i in password)):
-		print('Password is valid')    
-		return 'Password is valid'							
-	else:
-		raise AssertionError 
-		
+    if (len(password)>8):
+        if(password != None):
+            if(any(c.islower() for c in password)):
+                if(any(c.isupper() for c in password)):
+                    if(any(i.isdigit() for i in password)):
+                        return True
+                    else:
+                        raise Exception('Password has to contain a digit')
+                else:
+                    raise Exception('Password has to contain uppercase')
+            else:
+                raise Exception('Password has to contain lowercase')
+        else:
+            raise Exception('Password cannot be empty')
+
+    else:
+        raise Exception('Password has to be 8 characters long') 
+        
 def password_is_ok(password):
     count = 0
     if len(password)>8:
         count +=1
-    if password != None:
-        count +=1
-    if any(c.islower() for c in password):
-        count +=1
-    if any(c.isupper() for c in password):
-        count +=1
-    if any(i.isdigit() for i in password):
-        count +=1
+        if password != None:
+            count +=1
+            if any((c.islower() for c in password) or (any(c.isupper() for c in password)) or (i.isdigit() for i in password)):
+                count +=1
+    
     if count >= 3:
-        print('Password is okay')
-        return 'Password is okay'
+        return True
     else:
-        raise AssertionError
+        raise Exception('Password  either has to contain a digit  \n  or password has to contain uppercase \n or password has to contain lowercase') 
+
+
+
     
--- password_test.py
+++ password_test.py
@@ -1,30 +1,36 @@
 from password import password_is_valid, password_is_ok
-
+import pytest
 def test_password_is_valid():
-	try:
-		assert password_is_valid('QpA9Aapaaa') == 'Password is valid'
-		assert password_is_valid('Q1Aap9aaa') == 'Password is valid'
-		assert password_is_valid('QAAaupa8a') == 'Password is valid'
-	except AssertionError as e:
-		print('''               password should be larger than 8 chars
-               password should not be null
-               password should have at least one uppercase letter
-               password should have at least one lowercase letter
-               password should have at least one number''')
+	"""test that exception is raised for invalid passwords"""
+	with pytest.raises(Exception):
+		password_is_valid('qqqqqqqqq')
 
-	
+	with pytest.raises(Exception):
+		password_is_valid('')
+
+	with pytest.raises(Exception):
+		password_is_valid('Qqqqqqqqq')
+
+	"""test that valid passwords work"""	
+	password_is_valid('Q8qqqqqqqq') == True
+
+
 def test_password_is_okay():
-    try:
-        assert password_is_ok('QpAAapaa') == 'Password is okay'
-        assert password_is_ok('Q1AAapaaa') == 'Password is okay'
-        assert password_is_ok('QAAapa8a') == 'Password is okay'
-    except AssertionError as e:
-        print('''         Password must meet at least 3 of these conditions.       
-		    Password should be larger than 8 characters.
-            Password should not be null.
-            Password should have at least one uppercase letter.
-            Password should have at least one lowercase letter.
-            Password should have at least one number.''')
-        print(e)
+	"""test that exception is raised for invalid passwords"""
+	with pytest.raises(Exception):
+		password_is_ok('qqqqqqqq')
+
+	with pytest.raises(Exception):
+		password_is_ok('')
+
+	"""test that valid passwords work"""
+	password_is_ok('Q8qqqqqqqq') == True
+	password_is_ok('q8qqqqqqqq') == True
+	password_is_ok('Qqqqqqqqqq') == True
+	password_is_ok('qqqqqqqqqq') == True
+
+
+
+
 
 
